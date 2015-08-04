8fee14bdd93789c6c26245b45773431654a5cc7c
hto <hto@usc.edu>
2015-08-04 12:48:20 -0700

    add a spatial event detection

--- src/detect_events.py
+++ src/detect_events.py
@@ -6,84 +6,136 @@ from geojson import Feature, Point, FeatureCollection
 import requests
 import json
 import sys
+import math
 
 
-url = "http://mediaq.usc.edu/MediaQ_MVC_V3/api"
+url = "http://local.eclipse.org/MediaQ_MVC_V3/api"
 validate_endpoint = 'http://geojsonlint.com/validate'
 
-class Sequence(object):
-    start, end, min, max, pos_min, pos_max = None, None, None, None, None, None
+# An abstract event
+class Event(object):
+    start, end = None, None
 
     def __str__(self):
-        return str(self.start) + '\t' + str(self.end) + '\t' + str(self.min) + '\t' + str(self.max) + '\t' + str(self.pos_min) + '\t' + str(self.pos_max)
+        return str(self.start) + '\t' + str(self.end)
 
-    def __init__(self, start, end, min, max, pos_min, pos_max):
+    def __init__(self, start, end):
         self.start = start
         self.end = end
+
+
+# A directional event
+class DirectionalEvent(Event):
+    min, max, pos_min, pos_max = None, None, None, None
+
+    def __str__(self):
+        return str(Event.__str__(self)) + '\t' + str(self.min) + '\t' + str(self.max) + '\t' + str(self.pos_min) + '\t' + str(self.pos_max)
+
+    def __init__(self, start, end, min, max, pos_min, pos_max):
+        Event.__init__(self, start, end)
         self.min = min
         self.max = max
         self.pos_min = pos_min
         self.pos_max = pos_max
 
+class SpatialEvent(Event):
+
+    def __init__(self, start, end, min, max, pos_min, pos_max):
+        Event.__init__(self, start, end)
+
+def distance_km(lat1, lon1, lat2, lon2):
+    """
+    Distance between two geographical locations
+    """
+    R = 6371  # km
+    dLat = math.radians(abs(lat2 - lat1))
+    dLon = math.radians(abs(lon2 - lon1))
+    lat1 = math.radians(lat1)
+    lat2 = math.radians(lat2)
+
+    a = math.sin(dLat / 2) * math.sin(dLat / 2) + math.sin(dLon / 2) * math.sin(dLon / 2) * math.cos(lat1) * math.cos(
+        lat2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    d = R * c
+    return d
+
+SPATIAL_DISTANCE = 0.001    # km
+SPATIAL_MIN_LENGTH = 20
+
+def events_by_locality(dirs):
+    results = []
+    for i in range(len(dirs)):
+        curr = dirs[i]
+        if i == 0:
+            event = Event(0, 0)   # (start, end)
+        elif distance_km(dirs[event.start][0], dirs[event.start][1], curr[0], curr[1]) <= SPATIAL_DISTANCE:    # within range
+            event.end = i         # update end
+        else:   # new range
+            if event.end - event.start >= SPATIAL_MIN_LENGTH:
+                results.append(event)
+            # new event
+            event = Event(i, i)
+    return results
 
 
-RANGE = 10
-MIN_LENGTH = 50
+DIRECTIONAL_RANGE = 10
+MIN_LENGTH = 10
 def events_by_direction(dirs):
     results = []
     for i in range(len(dirs)):
         curr = dirs[i]
         if i == 0:
-            sequence = Sequence(0, 0, curr, curr, 0, 0)   # (start, end, min, max, pos_min, pos_max)
+            sequence = DirectionalEvent(0, 0, curr, curr, 0, 0)   # (start, end, min, max, pos_min, pos_max)
         elif sequence.min <= curr <= sequence.max:    # within range
             sequence.end = i         # update end
-        elif sequence.max - RANGE <= curr < sequence.min:  # left
+        elif sequence.max - DIRECTIONAL_RANGE <= curr < sequence.min:  # left
             sequence.end, sequence.min, sequence.pos_min = i, curr, i  # update min, pos_min
-        elif sequence.max < curr <= sequence.min + RANGE:
+        elif sequence.max < curr <= sequence.min + DIRECTIONAL_RANGE:
             sequence.end, sequence.max, sequence.pos_max = i, curr, i  # update max, pos_max
-        elif sequence.min - RANGE < curr < sequence.max - RANGE:
+        elif sequence.min - DIRECTIONAL_RANGE < curr < sequence.max - DIRECTIONAL_RANGE:
             if sequence.end - sequence.start >= MIN_LENGTH - 1:  # direction event is detected
                 results.append(sequence)
             # update max
             pos_max = i
             max = curr
             for j in reversed(range(sequence.start, i)):
-                if dirs[j] > max and dirs[j] - curr <= RANGE:
+                if dirs[j] > max and dirs[j] - curr <= DIRECTIONAL_RANGE:
                     max, pos_max = dirs[j], j
-                if dirs[j] - curr <= RANGE:
+                if dirs[j] - curr <= DIRECTIONAL_RANGE:
                     continue
                 else:
                     break
-            sequence = Sequence(j + 1, i, curr, max, i, pos_max)
-        elif sequence.min + RANGE < curr < sequence.max + RANGE:
+            sequence = DirectionalEvent(j + 1, i, curr, max, i, pos_max)
+        elif sequence.min + DIRECTIONAL_RANGE < curr < sequence.max + DIRECTIONAL_RANGE:
             if sequence.end - sequence.start >= MIN_LENGTH - 1:  # direction event is detected
                 results.append(sequence)
             # update min
             pos_min = i
             min = curr
             for j in reversed(range(sequence.start, i)):
-                if dirs[j] < min and curr - dirs[j] <= RANGE:
+                if dirs[j] < min and curr - dirs[j] <= DIRECTIONAL_RANGE:
                     min, pos_min = dirs[j], j
-                if curr - dirs[j] <= RANGE:
+                if curr - dirs[j] <= DIRECTIONAL_RANGE:
                     continue
                 else:
                     break
-            sequence = Sequence(j + 1, i, min, curr, pos_min, i)
+            sequence = DirectionalEvent(j + 1, i, min, curr, pos_min, i)
 
-        elif curr <= sequence.min - RANGE or curr >= sequence.max + RANGE:    # out of range
+        elif curr <= sequence.min - DIRECTIONAL_RANGE or curr >= sequence.max + DIRECTIONAL_RANGE:    # out of range
             if sequence.end - sequence.start >= MIN_LENGTH - 1:  # direction event is detected
                 results.append(sequence)
-            sequence = Sequence(i, i, curr, curr, i, i)
+            sequence = DirectionalEvent(i, i, curr, curr, i, i)
 
     return results
 
 
 
 # dirs = [120, 150, 160, 140, 150, 145, 135, 130, 150, 120]
+# dirs = [181.94008, 182.37218, 171.61913, 160.90152, 157.55756, 153.66913, 156.21973, 175.0801, 159.62263, 153.34453, 151.51772, 151.01799, 152.5305, 155.17336, 154.8254, 150.19846, 166.72073, 158.32208, 148.25983, 154.22003, 162.4578, 167.7894, 181.1051, 170.45929, 175.29805, 175.39275, 186.90277, 188.76363, 200.17162, 200.49422, 194.32492, 197.19994, 202.31071, 204.23193, 197.63225, 198.6546, 205.44516, 198.59033, 200.10536, 200.5626, 202.47163, 204.49399, 204.32697, 205.05452, 209.90833, 210.01016, 220.22246, 240.85333, 247.69794, 254.44272, 265.39795, 270.58826, 274.80835, 270.61554, 266.99905, 194.38068, 161.73622, 168.23277, 176.72787, 187.61053, 204.8588]
+# print dirs[31:43]
 # results = events_by_direction(dirs)
 # for s in results:
 #     print s
-
 
 
 
@@ -108,6 +160,7 @@ geoq = sm.GeoqApi(sm.ApiClient(url, "X-API-KEY", "8b51UFM2SlBltx3s6864eUO1zSoefe
 # swlat=34.019972, swlng=-118.291588, nelat=34.021111, nelng=-118.287125
 fc_videos = geoq.rectangle_query(swlat=34.018212, swlng=-118.291716, nelat=34.025296, nelng=-118.279826, startdate="2014-04-12 10:00:00",enddate="2014-04-13 23:59:59")
 fc_videos = fc_videos.replace('u\'','\"').replace('\'','\"')
+
 # validate GEOJSON
 geojson_validation = requests.post(validate_endpoint, data=fc_videos)
 if geojson_validation.json()['status'] != 'ok':
@@ -119,6 +172,7 @@ print "Number of videos: " + str(len(fc_videos.features))
 for video in fc_videos.features:
     if video.properties['vid']:
         vid = str(video.properties['vid'])
+	videoid = str(video.properties['videoid'])
         fovs = []
         # Returns a set of video frames
         try:
@@ -136,7 +190,15 @@ for video in fc_videos.features:
             # print vid
             trajectory = [(fov.geometry.coordinates[0],fov.geometry.coordinates[0],float(fov.properties['theta_x'])) for fov in fovs.features]
             dirs = [dir[2] for dir in trajectory]
-            results = events_by_direction(dirs)
+            locs = [(dir[0], dir[1]) for dir in trajectory]
+	        #print dirs
+            # results = events_by_direction(dirs)
+            # for s in results:
+            #     print len(dirs), s
+                # print geoq.video_segment_url(vid, s.start, s.end)
 
+
+            results = events_by_locality(locs)
             for s in results:
-                print vid, s
+                print s
+                print geoq.video_segment_url(vid, s.start, s.end)
